5a3bf4dc49f06369a46a502f79addc8bdb2e95d3
Zachary Smith <billyzacsmith@gmail.com>
2015-10-26 16:00:51 -0600

    Add jQuery and demonstrate setting clip-path with jQuery.

---
+++ app.js
@@ -0,0 +1,7 @@
+var x = 30;
+$(".shard").css("-webkit-clip-path", "polygon(" + x + "% 66%, 40.75% 84.429%, 47.85% 80%)")
+
+// $('.shard').css('background-color', 'tomato')
+$(".shard").animate({
+  opacity: .1
+ }, function(){})
