a04a21a4e54a1b5dfa74188ce70295ab7ccf85cb
Zachary Smith <billyzacsmith@gmail.com>
2015-10-29 15:08:21 -0600

    Transition to another shape with window is scrolled.

--- app.js
+++ app.js
@@ -13,5 +13,8 @@ console.log(p1)
 // State 1
 $('.shard').css('-webkit-clip-path', 'polygon('+ p1.x + '% ' + p1.y + '% ,30% 0% ,0% 50%)')
 
-p1.x = 10
-$('.shard').click().css('-webkit-clip-path', 'polygon('+ p1.x + '% ' + p1.y + '% ,30% 0% ,0% 50%)')
+// Show state 2 when the window is scrolled
+$( window ).scroll(function() {
+  p1.x = 10
+  $( ".shard" ).css('-webkit-clip-path', 'polygon('+ p1.x + '% ' + p1.y + '% ,30% 0% ,0% 50%)')
+})
