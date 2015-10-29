42bedda50dd69a1f802a0df8014121d11f217ad2
Zachary Smith <billyzacsmith@gmail.com>
2015-10-28 19:47:56 -0600

    Represent a point on the polygon with an object instead of an explicit number.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-$('.shard').click().toggleClass('state1')
+var point = {
+  x: 50,
+  y: 30
+}
+
+// $('.shard').click().toggleClass('state1')
+
+$('.shard').css('-webkit-clip-path', 'polygon('+ point.x + '% ' + point.y + '% ,30% 0% ,0% 50%)')
