0a2fbbbce3b3c6a088378b0985bb8e6c209beb11
Zachary Smith <billyzacsmith@gmail.com>
2015-10-29 15:06:14 -0600

    Make constructor function for Point. Instantiate one point, p1.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-var point = {
-  x: 50,
-  y: 30
+function Point(x, y) {
+  this.x = x
+  this.y = y
 }
 
-// $('.shard').click().toggleClass('state1')
 
-$('.shard').css('-webkit-clip-path', 'polygon('+ point.x + '% ' + point.y + '% ,30% 0% ,0% 50%)')
+
+
+
+var p1 = new Point(50, 30)
+console.log(p1)
+
+// State 1
+$('.shard').css('-webkit-clip-path', 'polygon('+ p1.x + '% ' + p1.y + '% ,30% 0% ,0% 50%)')
+
+p1.x = 10
+$('.shard').click().css('-webkit-clip-path', 'polygon('+ p1.x + '% ' + p1.y + '% ,30% 0% ,0% 50%)')
