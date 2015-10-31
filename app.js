4a9e02cb858ed10077f167be3114196fc4eec853
Zachary Smith <billyzacsmith@gmail.com>
2015-10-31 15:00:17 -0600

    Add another div, another triangle. Make divs slightly transparent so both can be seen.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var p4 = new Point(40, 90);
 var p5 = new Point(10, 95);
 
 var t1 = new Triangle(p1, p2, p3);
-var t2 = new Triangle(p4, p1, p5);
+var t2 = new Triangle(p2, p1, p5);
 
 // Takes class name of div (don't pass the dot) and clips it with triangle t
 var drawShape = function(div, t) {
@@ -26,15 +26,15 @@ $('.' + div).css('-webkit-clip-path', 'polygon('
   + t.p3.x + '% ' + t.p3.y + '%)')
 }
 
-drawShape('shard', t1)
-
+drawShape('shard1', t1)
+drawShape('shard2', t2)
 
 // Show state 2 when the window is scrolled
-$( window ).scroll(function() {
-  p1.x = 7
-  p1.y = 20
-  $( ".shard" ).css('-webkit-clip-path', 'polygon('+ p1.x + '% ' + p1.y + '% ,30% 50% ,0% 50%)')
-})
+// $( window ).scroll(function() {
+//   p1.x = 7
+//   p1.y = 20
+//   $( ".shard" ).css('-webkit-clip-path', 'polygon('+ p1.x + '% ' + p1.y + '% ,30% 50% ,0% 50%)')
+// })
 
 
 // var points = [];
